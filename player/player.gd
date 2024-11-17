98a38eac06f0d421b064d862c1d91c5d3d3108ea
Allendale Nato <natoallendale@gmail.com>
2024-11-17 10:04:35 -0600

    Add main game scene and background

--- player/player.gd
+++ player/player.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 @onready var HappyBoo: Node2D = %HappyBoo
 
 func _physics_process(delta: float) -> void:
+	# Fetch input and move player
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
 	
+	# Walk animation
 	if velocity != Vector2.ZERO:
 		HappyBoo.play_walk_animation()
 	else:
