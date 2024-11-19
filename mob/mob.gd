cf0910b1c9712ff010e9a9cb33ffa98607c0ecfb
Allendale Nato <natoallendale@gmail.com>
2024-11-18 21:10:46 -0600

    Add mob animation and player camera

--- mob/mob.gd
+++ mob/mob.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 @onready var player:= %Player
+@onready var slime:= $Slime
 @export var slime_speed:= 300
+
+func _ready() -> void:
+	slime.play_walk()
 
 func _physics_process(delta: float) -> void:
 	var direction:= global_position.direction_to(player.global_position)
