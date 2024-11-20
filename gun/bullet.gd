e4ab845867acc756d0618d443503d281f50e974e
Allendale Nato <natoallendale@gmail.com>
2024-11-19 21:16:05 -0600

    Connect bullet scene's body entered signal

--- gun/bullet.gd
+++ gun/bullet.gd
@@ -4,6 +4,8 @@ extends Area2D
 @export var BULLET_MAX_DIST := 1200.0
 var travelled_distance := 0.0
 
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
 
 func _physics_process(delta: float) -> void:
 	var direction := Vector2.RIGHT.rotated(rotation)
@@ -12,3 +14,7 @@ func _physics_process(delta: float) -> void:
 	travelled_distance += BULLET_SPEED * delta
 	if travelled_distance > BULLET_MAX_DIST:
 		queue_free()
+
+func _on_body_entered(body: Node2D):
+	queue_free()
+	pass
